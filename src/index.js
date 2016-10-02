d4ac8d68b708449c0bfba50c7767abf31fafbbe8
Trace Harris <traceh@whiletrace.com>
2016-10-01 17:31:00 -0700

    deleted obsolete route within react router all tests passing

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, hashHistory } from 'react-router'
 import Main from 'components/Main'
-import Error from 'components/Error'
 import Todo from 'components/Todo'
 import TodosReducer from 'redux/Todo_redux'
 
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={Main} />
-    <Route path="/Error" component={Error} />
     <Route path="/Todo" component={Todo} />
     <Route path="redux/Todo_redux"component={TodosReducer} />
   </Router>
