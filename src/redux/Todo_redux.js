b48154f08af420d2d9836819f6f7f56b63437d0c
Trace Harris <traceh@whiletrace.com>
2016-12-07 14:40:55 -0800

    normalized state shape and extracted singleTodo reducer module

--- src/redux/Todo_redux.js
+++ src/redux/Todo_redux.js
@@ -1,41 +1,54 @@
 // importing neccessary node modules
-// createSTore
 // and combine Reducers
-
+import { combineReducers } from 'redux'
+import singleTodo from './singleTodo_reduce'
 // this reducer was extracted from original reducer renamed todos
 // in doing so began implementing reducer compositional pattern
 // takes state and action as arguments governs individual todos
-const todo = (state, action) => {
+
 // switch statement which takes the type property of the action object as a argument
-  switch (action.type) {
 // case for type property ADD_TODO
-    case 'ADD_TODO':
 // returns an object with keys of id, text, and completed
 // with values corresponding to the relevant action object
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false,
-      }
 // for the case of TOGGLE_TODO
-    case 'TOGGLE_TODO':
 // if the key id of the action object does not match the
 // key in the state object
 // it will return the state object
-      if (state.id !== action.id) {
-        return state
-      }
 // if the action object id being evaluated and the state object id
 // will return the opposite value of the completed key of the state object
+
+
+const byId = (state = {}, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+    case 'TOGGLE_TODO':
       return {
         ...state,
-        completed: !state.completed,
+        [action.id]: singleTodo(state, [action.id], action),
       }
-
     default:
       return state
   }
 }
+
+const allIds = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, action.id]
+    default:
+      return state
+  }
+}
+const getAllTodos = (state) =>
+  state.allIds.map(id => state.byId[id])
+
+
+const todos = combineReducers({
+  byId,
+  allIds,
+})
+
+
  /*
  const visibilityFilter = (
   state = 'SHOW_ALL',
@@ -48,6 +61,8 @@ const todo = (state, action) => {
   }
 }
 */
+
+/*
 // this is original reducer that todo was extracted from
 // takes state which is an empty array and action as arguments
 const todos = (state = [], action) => {
@@ -66,11 +81,12 @@ const todos = (state = [], action) => {
 // returns a  new array using the mapping method on the state
 // array which calls the todo reducer on each index of the
 // state array
-      return state.map(t => todo(t, action))
+      return state.map(t => singleTodo(t, action))
     default:
       return state
   }
 }
+*/
 // using a built in redux function called combine reducers
 // combines the reducers todos, and visibility to one reducer named TodoApp
 
@@ -84,21 +100,22 @@ export default todos
 // to be rendered by the ui
 
 export const getVisibleTodos = (state, filter) => {
+  const allTodos = getAllTodos(state)
 // switch using the filter  as an argument
   switch (filter) {
 // in the case of SHOW_ALL
     case 'all':
 // returns all the todos
-      return state
+      return allTodos
 // for the case SHOW_COMPLETED
     case 'completed':
 // will return the todos that have the key completed with the value true
-      return state.filter(
+      return allTodos.filter(
       t => t.completed)
 // In the case show active will show the todos
 // with the key completed with the value false
     case 'active':
-      return state.filter(
+      return allTodos.filter(
         t => !t.completed)
     default:
       throw new Error('Unknown filter: ${filter}.')
